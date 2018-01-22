3a01fb0d276f4debb4c63ed0ba26c24ad5548886
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2018-01-23 01:43:20 +0200

    initial

---
+++ Samples/NETCORE.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PublishSubscribe;
+
+namespace NETCORE.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
